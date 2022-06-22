e0bea1afe16d75a8902b95b3c739b81c4fe382d2
Simeone Umoh <simeoneumoh@outlook.com>
2022-06-22 21:19:54 +0100

    BookRequest BorrowedBook models

--- library_api/library/models.py
+++ library_api/library/models.py
@@ -1,11 +1,45 @@
+from unicodedata import category
 from django.db import models
+from users.models import Manager, Student
 
 
 class BookCategory(models.Model):
-    """Admin Model"""
+    """BookCategory Model"""
     name = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField("updated_at", auto_now=True)
 
     class Meta:
         ordering = ['-created_at']
+
+class Book(models.Model):
+    """Book Model"""
+    name = models.TextField(null=True)
+    author = models.TextField(null=True)
+    date_published = models.DateField(null=True)
+    category = models.ForeignKey(BookCategory, null=True, on_delete=models.SET_NULL)
+    is_borrowed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField("updated_at", auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class BookRequest(models.Model):
+    """BookRequest Model"""
+    book = models.ForeignKey(Book, null=True, on_delete=models.SET_NULL)
+    student = models.ForeignKey(Student, null=True, on_delete=models.SET_NULL)
+    status = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField("updated_at", auto_now=True)
+    class Meta:
+        ordering = ['-created_at']
+
+class BorrowedBook(models.Model):
+    """BorrowedBook Model"""
+    book = models.ForeignKey(Book, null=True, on_delete=models.SET_NULL)
+    student = models.ForeignKey(Student, null=True, on_delete=models.SET_NULL)
+    borrowed_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-borrowed_at']
