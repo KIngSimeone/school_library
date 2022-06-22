a0a48d291856d50703fa3ffea5984eaa536a71dd
Simeone Umoh <simeoneumoh@outlook.com>
2022-06-22 21:02:36 +0100

    Admin model

--- library_api/library/models.py
+++ library_api/library/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Admin(models.Model):
+    """Admin Model"""
+    first_name = models.TextField(null=True)
+    last_name = models.TextField(null=True)
+    email_address = models.EmailField(null=True)
+    user_name = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField("updated_at", auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
