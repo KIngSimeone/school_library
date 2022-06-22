5c1e9aa9db2b999360c64638f37aae53276fe22d
Simeone Umoh <simeoneumoh@outlook.com>
2022-06-22 21:09:41 +0100

    BookCategory models

--- library_api/library/models.py
+++ library_api/library/models.py
@@ -1,35 +1,9 @@
 from django.db import models
 
-class Admin(models.Model):
+
+class BookCategory(models.Model):
     """Admin Model"""
-    first_name = models.TextField(null=True)
-    last_name = models.TextField(null=True)
-    user_name = models.TextField(null=True)
-    password = models.TextField(null=True, blank=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField("updated_at", auto_now=True)
-
-    class Meta:
-        ordering = ['-created_at']
-
-class Manager(models.Model):
-    """Manager Model"""
-    first_name = models.TextField(null=True)
-    last_name = models.TextField(null=True)
-    user_name = models.TextField(null=True)
-    password = models.TextField(null=True, blank=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField("updated_at", auto_now=True)
-
-    class Meta:
-        ordering = ['-created_at']
-
-class Student(models.Model):
-    """Manager Model"""
-    first_name = models.TextField(null=True)
-    last_name = models.TextField(null=True)
-    user_name = models.TextField(null=True)
-    password = models.TextField(null=True, blank=False)
+    name = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField("updated_at", auto_now=True)
 
--- library_api/users/models.py
+++ library_api/users/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Admin(models.Model):
+    """Admin Model"""
+    first_name = models.TextField(null=True)
+    last_name = models.TextField(null=True)
+    user_name = models.TextField(null=True)
+    password = models.TextField(null=True, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField("updated_at", auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+class Manager(models.Model):
+    """Manager Model"""
+    first_name = models.TextField(null=True)
+    last_name = models.TextField(null=True)
+    user_name = models.TextField(null=True)
+    password = models.TextField(null=True, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField("updated_at", auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+class Student(models.Model):
+    """Manager Model"""
+    first_name = models.TextField(null=True)
+    last_name = models.TextField(null=True)
+    user_name = models.TextField(null=True)
+    password = models.TextField(null=True, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField("updated_at", auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
