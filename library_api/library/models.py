acf3f325de27658331b425e281aed519af5c5b5f
Simeone Umoh <simeoneumoh@outlook.com>
2022-06-22 21:04:38 +0100

    Manager model

--- library_api/library/models.py
+++ library_api/library/models.py
@@ -4,8 +4,20 @@ class Admin(models.Model):
     """Admin Model"""
     first_name = models.TextField(null=True)
     last_name = models.TextField(null=True)
-    email_address = models.EmailField(null=True)
     user_name = models.TextField(null=True)
+    password = models.TextField(null=True, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField("updated_at", auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+class Manager(models.Model):
+    """Manager Model"""
+    first_name = models.TextField(null=True)
+    last_name = models.TextField(null=True)
+    user_name = models.TextField(null=True)
+    password = models.TextField(null=True, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField("updated_at", auto_now=True)
 
