cf296d9d4a74cb297aa9c20b1f0f8a651398e8e7
Simeone Umoh <simeoneumoh@outlook.com>
2022-06-23 13:44:07 +0100

    AdminView get

--- library_api/users/views.py
+++ library_api/users/views.py
@@ -5,14 +5,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from api_utility.views import success_response, paginated_response, created_response
 from .serializers import AdminSerializer
-from .utils import create_admin
-# class HelloView(APIView):
-#     permission_classes = (IsAuthenticated,)
-
-#     def get(self, request):
-#         content = {'message': 'Hello, World!'}
-#         return Response(content)
-
+from .utils import (
+    create_admin, get_all_admins
+)
+from django.core.paginator import Paginator
 class AdminView(APIView):
     """
     List all Admins or create a new admin
@@ -31,5 +27,30 @@ class AdminView(APIView):
         serializer = AdminSerializer(admin)
         data = serializer.data
         return created_response(message="successfully created admin", body=data)
+    
+    def get(self, request):
+        admins = get_all_admins()
+        queryDict = request.GET
+
+        # Paginate the retrieved users
+        page_by = queryDict.get('page_by') if 'page_by' in queryDict else 10
+        page = queryDict.get('page') if 'page' in queryDict else 1
+
+        paginator = Paginator(admins, page_by)
+        paginationDetails = {
+            "totalPages": paginator.num_pages,
+            "limit": page_by,
+            "count": paginator.count,
+            "currentPage": page
+        }
+        serializer = AdminSerializer(admins, many=True)
+        data = serializer.data
+
+        return paginated_response(
+            message="successfully retrieved admins",
+            body=data,
+            pagination=paginationDetails
+        )
+
 
 
