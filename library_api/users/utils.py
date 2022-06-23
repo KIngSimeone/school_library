a57df7b8e69a8a8c36f277a0bdd33ac46f3e11eb
Simeone Umoh <simeoneumoh@outlook.com>
2022-06-23 13:34:09 +0100

    AdminView create updaes

---
+++ library_api/users/utils.py
@@ -0,0 +1,24 @@
+from .models import Admin
+from django.contrib.auth.hashers import make_password
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+
+def create_admin(first_name, last_name, user_name, password):
+    try:
+        admin = Admin.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            user_name=user_name,
+            password=make_password(password),
+        )
+        return admin
+
+    except Exception as e:
+        logger.error("create_admin@error")
+        logger.error(e)
+        return None
