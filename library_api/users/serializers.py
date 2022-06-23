f12ffebc75afdc6d91d87bc2bfcafad058885aa2
Simeone Umoh <simeoneumoh@outlook.com>
2022-06-23 14:26:54 +0100

    StudentSerializer ManagerSerializer

--- library_api/users/serializers.py
+++ library_api/users/serializers.py
@@ -1,9 +1,21 @@
-from .models import Admin
+from .models import Admin, Manager, Student
 from rest_framework import serializers
 
 
 class AdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = Admin
+        fields = ['id', 'first_name', 'last_name', 'user_name',
+                  'created_at', 'updated_at']
+
+class ManagerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Manager
+        fields = ['id', 'first_name', 'last_name', 'user_name',
+                  'created_at', 'updated_at']
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
         fields = ['id', 'first_name', 'last_name', 'user_name',
                   'created_at', 'updated_at']
