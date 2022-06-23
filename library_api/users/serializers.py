ce78505ee07b988b5793f1e36aea1cb464c4ad6f
Simeone Umoh <simeoneumoh@outlook.com>
2022-06-23 13:26:57 +0100

    AdminSerializer

--- library_api/users/serializers.py
+++ library_api/users/serializers.py
@@ -6,4 +6,4 @@ class AdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = Admin
         fields = ['id', 'first_name', 'last_name', 'user_name',
-                  'created_at', 'updated_at',]
+                  'created_at', 'updated_at']
