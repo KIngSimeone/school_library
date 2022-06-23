c1d2970142c9f68e1482adcb2f172816f92aaf19
Simeone Umoh <simeoneumoh@outlook.com>
2022-06-23 13:12:40 +0100

    updates

--- library_api/library_api_config/settings.py
+++ library_api/library_api_config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'library',
+    'api_utility,'
 ]
 
 MIDDLEWARE = [
--- library_api/users/views.py
+++ library_api/users/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-
+from api_utility.views import success_response, paginated_response, created_response
 
 # class HelloView(APIView):
 #     permission_classes = (IsAuthenticated,)
